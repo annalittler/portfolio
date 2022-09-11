7c80a7673109c8dceb3177523b41376d79ac485f
annalittler <77206754+annalittler@users.noreply.github.com>
2022-09-11 13:59:15 +1200

    placeholder complete

---
+++ src/Pages/Home-Page/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>annalittler.com</h2>
+      <h1>coming soon</h1>
+      <span>
+        <a href="https://github.com/annalittler" target="_blank">
+          github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anna-littler-00032a237/"
+          target="_blank"
+        >
+          linkedin
+        </a>
+      </span>
+    </div>
+  );
+}
